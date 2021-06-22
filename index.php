30dc3954bc5e058291ec4488d844b88e03dd197a
Paurush Sinha <76876045+sinhapaurush@users.noreply.github.com>
2021-06-22 18:36:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,18 +12,6 @@
         
     </div>
 </div>
-<script>
-    function progress(title, message){
-        document.getElementById("progresstitle").innerHTML = title;
-        document.getElementById("progressmessage").innerHTML = message;
-        document.getElementById("overlaytmincprogress").style.display='block';
-    }
-    function dissmiss_progress(){
-        document.getElementById("progresstitle").innerHTML = "";
-        document.getElementById("progressmessage").innerHTML = "";
-        document.getElementById("overlaytmincprogress").style.display='none';
-    }
-</script>
 <?php
     function progress($title, $message){
         ?><script>progress("<?php echo $title; ?>", "<?php echo $message; ?>");</script><?php

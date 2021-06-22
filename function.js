e0dc58dab4dca5c3b6e6dfc5232dbfad03fa3f09
Paurush Sinha <76876045+sinhapaurush@users.noreply.github.com>
2021-06-22 18:36:39 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,1 +1,10 @@
-
+    function progress(title, message){
+        document.getElementById("progresstitle").innerHTML = title;
+        document.getElementById("progressmessage").innerHTML = message;
+        document.getElementById("overlaytmincprogress").style.display='block';
+    }
+    function dissmiss_progress(){
+        document.getElementById("progresstitle").innerHTML = "";
+        document.getElementById("progressmessage").innerHTML = "";
+        document.getElementById("overlaytmincprogress").style.display='none';
+    }

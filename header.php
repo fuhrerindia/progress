3d5d81b3579dcbbe6554fc636dbe0dd2508306c3
Paurush Sinha <76876045+sinhapaurush@users.noreply.github.com>
2021-06-22 18:38:02 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,2 +1,3 @@
-<!--PROGRESS POPUP DESIGN FOR YUGAL-->
+<!--PROGRESS POPUP FILES FOR YUGAL-->
 <link rel="stylesheet" type="text/css" href="lib/progress/design.css">
+<script src="lib/progress/function.js"></script>
